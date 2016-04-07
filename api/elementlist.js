0619761cac97ed4578372d793d7246616745efd2
Junaid Atari <mj.atari@gmail.com>
2016-04-08 04:54:25 +0500

    api added

---
+++ api/elementlist.js
@@ -0,0 +1,3 @@
+
+var ApiGen = ApiGen || {};
+ApiGen.elements = [["c","yii2cdn\\Cdn"],["c","yii2cdn\\Component"],["c","yii2cdn\\ConfigFile"],["c","yii2cdn\\ConfigLoader"],["c","yii2cdn\\ConfigParser"],["c","yii2cdn\\File"],["c","yii2cdn\\Section"]];
