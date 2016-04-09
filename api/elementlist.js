aa3d0878c7ad372337d0f042bf6f1d5f9078eb55
Junaid Atari <mj.atari@gmail.com>
2016-04-10 00:53:39 +0500

    api updated to latest changes

--- api/elementlist.js
+++ api/elementlist.js
@@ -1,2 +1,3 @@
+
 var ApiGen = ApiGen || {};
-ApiGen.elements = [["c", "yii2cdn\\Cdn"], ["c", "yii2cdn\\Component"], ["c", "yii2cdn\\ConfigFile"], ["c", "yii2cdn\\ConfigLoader"], ["c", "yii2cdn\\ConfigParser"], ["c", "yii2cdn\\File"], ["c", "yii2cdn\\Section"]];
+ApiGen.elements = [["c","yii2cdn\\Cdn"],["c","yii2cdn\\Component"],["c","yii2cdn\\ConfigFile"],["c","yii2cdn\\ConfigLoader"],["c","yii2cdn\\ConfigParser"],["c","yii2cdn\\File"],["c","yii2cdn\\Section"]];
